1615f8c13d58670b80305b33fea78403abcee1e9
Nosp1 <Trymstaur@hotmail.com>
2019-03-20 15:31:54 +0100

    fixed ignore to include iml files for compatability across microsoft and mac

--- main_slices.go
+++ main_slices.go
@@ -8,10 +8,10 @@ import (
 func main() {
 
 	var byteSlice1 = slice.AllocateVar(100)
-	fmt.Println("&byteslice1[0]")
+	fmt.Println(" &byteslice1[0]")
 	fmt.Println(&byteSlice1[0])
 	aslice := slice.Reslice(byteSlice1, 50, 100)
-	fmt.Println("&aslice[0]")
+	fmt.Println(" &aslice[0]")
 	fmt.Println(&aslice[0])
 	fmt.Println("&byteslice1[50]")
 	fmt.Println(&byteSlice1[50])
