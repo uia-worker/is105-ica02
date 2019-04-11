17a19739b9d93fb3ca77f5a9e387528666123a97
Nosp1 <Trymstaur@hotmail.com>
2019-04-11 17:33:44 +0200

    fikset på utprint formatet til iso.go

--- main_iso.go
+++ main_iso.go
@@ -1,11 +1,14 @@
 package main
 
-import "./iso"
+import (
+	"./iso"
+	"fmt"
+)
 
 func main() {
 
 	extendedASCIIStringLiteral := iso.GetExtendedASCIIStringLiteral()
 	iso.IterateOverASCIIStringLiteral(extendedASCIIStringLiteral)
-
+	fmt.Println("Under kommer stringen: \"Salut, ça va °-) Κοστίζει €50\"")
 	iso.GreetingExtendedASCII()
 }
