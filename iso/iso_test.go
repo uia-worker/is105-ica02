0fc80836ac53b64e2a38fe0966795a91bd11f059
bjornar.risdalen <bwendelborgrisdalen@gmail.com>
2019-04-30 14:17:47 +0200

    No changes to ascii.go that I can spot. Changed the test-file for iso

--- iso/iso_test.go
+++ iso/iso_test.go
@@ -1,37 +1,24 @@
 package iso
 
 import (
-	"fmt"
 	"testing"
 )
 
-var greetingtestExtendedASCII = []struct {
-	n1       string
-	expected string
-}{
-	{"Salut, ça va °-", ""},
-	{"Salut, ça va °-) Κοστίζει €50", ""},
-	{"Salut, ça va °-) Κοστίζει ​€50 Forstår du?​",""},
-}
-//test will fail as the expected string input - redundant test from inital deliverable.
-func TestGreetingtestextendedASCII(t *testing.T) {
-	for _, v := range greetingtestExtendedASCII{
-		if val := GreetingExtendedASCII(); val != v.expected{
-			t.Errorf(" greetingExtendedASCII( %q) returned %q expected %q",v.n1, val, v.expected)
+func TestGreetingExtendedASCII(t *testing.T) {
+	s := GreetingExtendedASCII()
+	isExtendedAscii := withinExtendedAscii(s)
 
-		}
+	if !isExtendedAscii {
+		t.Fatal("Not ExtendedASCII")
 	}
-
 }
-// test that test the string to see if its within the extended ASCII characters.
-func TestGreetingExtendedASCIIONLY(t *testing.T) {
-	val := GreetingExtendedASCII()
-	for _, i := range val {
-		if i < 0x7f && i > 0xff {
-			t.Errorf("greetingExtendedASCII() returns non-extended ASCII value: %q - %v", i, i)
-			fmt.Printf("%c", i)
-		}
-		fmt.Printf("%c", i)
-	}
 
+func withinExtendedAscii(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 255 {
+			return false
+		}
+
+	}
+	return true
 }
